b37b17346b4d8034048b78327ddd52b83ba7f481
Muthembwa <mwaniki.jacob@gmail.com>
2022-02-26 14:10:28 +0300

    Remove subjects from students

--- app/Student.php
+++ app/Student.php
@@ -19,42 +19,26 @@ class Student extends Model
     ];
 
     protected $fillable = [
-        'music',
-        'arabic',
-        'german',
-        'french',
-        'biology',
-        'team_id',
-        'english',
-        'physics',
-        'class_id',
-        'aviation',
-        'stream_id',
-        'geography',
-        'chemestry',
-        'kiswahili',
+        
+        'first_name',
         'last_name',
+        'class_id',
+        'stream_id',
+        'admission_number',
+        'guardians_name',
+        'guardians_phone_no', 
+        'guardians_email',       
         'updated_at',
         'created_at',
-        'first_name',
-        'deleted_at',
-        'agriculture',
-        'mathematics',
-        'home_science',
-        'art_and_design',
-        'guardians_name',
-        'guardians_email',
-        'computer_science',
-        'business_studies',
-        'admission_number',
-        'guardians_phone_no',
-        'religious_education',
-        'history_and_government',
+        'deleted_at'
+       
+        
+    
     ];
 
     function class()
     {
-        return $this->belongsTo(Darasa::class, 'class_id');
+        return $this->belongsTo(Darasa::class,'class_id');
     }
 
     public function stream()
